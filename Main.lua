858c32902e5d969b343efebd717998de6ad1e80d
Kenneth Porter <shiva+git@sewingwitch.com>
2024-10-15 14:36:36 -0700

    Tooltip line type 20 also qualifies. Added DevTool debugging to track that down.

--- Main.lua
+++ Main.lua
@@ -16,25 +16,44 @@ addon.color = "00c0ff"
 -- example string:
 --   Use: Study to increase your Dragon Isles mining Knowledge by 1
 
+local debugging = nil
+local misses = {} -- for DevTool debugging
+if debugging and DevTool then DevTool:AddData(misses, "BetterBagsProfessionKnowledge misses") end -- type /dev to view the table
+
+local function addMiss(data, reason)
+   if debugging and DevTool then
+      misses[data.itemInfo.itemName] = reason
+   end
+end
+
 categories:RegisterCategoryFunction("ProfessionKnowledgeFilter", function (data)
+
 
    -- avoid tooltip scan for older stuff
    if data.itemInfo.expacID < LE_EXPANSION_DRAGONFLIGHT then
+      addMiss(data, "wrong expacID")
       return nil
    end
 
    local tooltipInfo = C_TooltipInfo.GetOwnedItemByID(data.itemInfo.itemID)
-  
+   local found = nil
+
+   local lastTooltipLine = "no tooltip lines"
    for k,v in pairs(tooltipInfo.lines) do
-      if v.type == 0 then
+      if (v.type == 0) or (v.type == 20) then
          -- we cheat and assume expansion is two words. Fix pattern if that assumption breaks with a future expansion
          local found, _, expansion, profession, amount = string.find(v.leftText, "Use: Study to increase your (%a+ %a+) (%a+) Knowledge by (%d+)")
          -- addon:Print(data.itemInfo.itemName .. ": '" .. v.leftText .. "' " .. tostring(found))
          if found then
             return "|cff" .. addon.color .. "Knowledge - " .. profession .. " - " .. expansion .. "|r"
+         else
+            lastTooltipLine = v.leftText
          end
+      else
+         lastTooltipLine = "wrong type: " .. tostring(v.type)
       end
    end
 
+   addMiss(data, lastTooltipLine)
    return nil
 end)
